77a261597f8dc82e152769d5aea2fd133da16bd9
doniwirawan <doniwirawan166@gmail.com>
2023-01-26 11:13:03 +0800

    change the files directory

--- src/server.js
+++ src/server.js
@@ -1,46 +1,13 @@
 import express from "express"
-import mongoose from "mongoose"
-import runPageSpeed from "../dist/runPageSpeed.js"
 import connectDB from "../db/config.js"
-import Performance from "../model/performance.js"
+import performanceRoute from "../routes/performance.js"
 
 const app = express()
 
 // Connect to MongoDB
 connectDB()
 
-// Create a route to test the performance of a URL
-app.get("/performance", async (req, res) => {
-  const { url } = req.query
-
-  try {
-    if (!url) {
-      return res.status(400).send({ error: "You must provide a URL to test" })
-    }
-
-    const result = await runPageSpeed(url)
-
-    // Create a new Performance object
-    const data = {
-      url,
-      load_time: result.loadTime,
-      layout_duration: result.LayoutDuration,
-      script_duration: result.scriptDuration,
-      task_duration: result.taskDuration,
-      performance_score: result.performanceScore,
-      best_practice_score: result.bestPracticesScore,
-      seo_score: result.seoScore,
-    }
-    const performanceData = new Performance(data)
-
-    // Save the Performance object to the database
-    await performanceData.save()
-
-    return await res.status(200).send(data)
-  } catch (error) {
-    return res.status(500).send({ error: error.message })
-  }
-})
+app.use('/performance', performanceRoute)
 
 const port = process.env.PORT || 5000
 
