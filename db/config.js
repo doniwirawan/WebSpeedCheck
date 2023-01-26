fb870c4436719e3097f21579a780a4b78ec3e04b
doniwirawan <doniwirawan166@gmail.com>
2023-01-26 14:24:53 +0800

    adding .env

--- db/config.js
+++ db/config.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose"
+import dotenv from 'dotenv';
+dotenv.config();
 
-export default async function connectDB () {
-  // Connect to MongoDB
-  try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/web-performance", { useNewUrlParser: true })
-    console.log("Mongodb connected")
-  } catch (err) {
-    console.log(err)
-  }
+export default async function connectDB() {
+    // Connect to MongoDB
+    try {
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+        console.log("Mongodb connected")
+    } catch (err) {
+        console.log(err)
+    }
 }
